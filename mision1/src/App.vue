5a578b3e69962c660cb69f7f787978c87aaed102
arnauavanza <arnau@avanzaprl.es>
2026-06-16 12:51:05 +0200

    feat: template code edited to show asked content

--- mision1/src/App.vue
+++ mision1/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloUser from './components/HelloUser.vue/index.js'
 </script>
 
 <template>
-  <HelloWorld />
+  <HelloUser />
 </template>
---
+++ mision1/src/components/HelloUser.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+function changeName() {
+  let name: string = "Arnau"
+  let welcome : string = "Hola, " + name + "!"
+  let element = document.getElementById("welcome")
+  element.textContent = welcome
+}
+</script>
+
+<template>
+  <h1 id="welcome">Hola, usuario!</h1>
+  <button v-on:click="changeName()">Actualizar nombre</button>
+</template>
